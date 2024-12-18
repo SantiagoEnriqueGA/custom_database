ce86a5c7c5a4dd270c03f1b13a0af8c5cfed33f2
sg670w <sg670w@att.com>
2024-12-18 11:54:51 -0600

    Extracted pydocs css

--- scripts/documentation_html.ps1
+++ scripts/documentation_html.ps1
@@ -22,6 +22,44 @@ $files | ForEach-Object {
     pydoc -w $_
 } | Out-File -FilePath "scripts/documentation.txt" -Encoding utf8
 
+# Add custom CSS to each HTML file
+$cssContent = @"
+<style>
+body { background-color: #f0f0f8; }
+table.heading tr { background-color: #7799ee; }
+.decor { color: #ffffff; }
+.title-decor { background-color: #ffc8d8; color: #000000; }
+.pkg-content-decor { background-color: #aa55cc; }
+.index-decor { background-color: #ee77aa; }
+.functions-decor { background-color: #eeaa77; }
+.data-decor { background-color: #55aa55; }
+.author-decor { background-color: #7799ee; }
+.credits-decor { background-color: #7799ee; }
+.error-decor { background-color: #bb0000; }
+.grey { color: #909090; }
+.white { color: #ffffff; }
+.repr { color: #c040c0; }
+table.heading tr td.title, table.heading tr td.extra { vertical-align: bottom; }
+table.heading tr td.extra { text-align: right; }
+.heading-text { font-family: helvetica, arial; }
+.bigsection { font-size: larger; }
+.title { font-size: x-large; }
+.code { font-family: monospace; }
+table { width: 100%; border-spacing: 0; border-collapse: collapse; border: 0; }
+td { padding: 2; }
+td.section-title, td.multicolumn { vertical-align: bottom; }
+td.multicolumn { width: 25%; }
+td.singlecolumn { width: 100%; }
+</style>
+"@
+
+$generatedDocs | ForEach-Object {
+    $htmlContent = Get-Content -Path $_.FullName
+    $htmlContent = $htmlContent -replace "(<head>)", "`$1`n$cssContent"
+    $htmlContent = $htmlContent -replace "<td class=`"extra`">.*?</td>", ""
+    Set-Content -Path $_.FullName -Value $htmlContent
+}
+
 # Move all generated .html files to the docs/ folder
 $generatedDocs = Get-ChildItem -Recurse -Filter *.html
 $destinationFolder = "docs/"
