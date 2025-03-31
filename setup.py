6a504eb48a56f8ff11cf89565ec535417a3cd6d7
Santiago Gonzalez Angarita <50879742+SantiagoEnriqueGA@users.noreply.github.com>
2025-03-31 15:30:05 -0500

    feat: add setup for CLI executable

--- setup.py
+++ setup.py
@@ -4,17 +4,21 @@ setup(
     name="segadb",
     version="0.1.0",
     packages=find_packages(),
-    install_requires=[],
     author="Santiago Gonzalez",
     author_email="sega97@gmail.com",
     description="A simple database library built in Python",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url="https://https://github.com/SantiagoEnriqueGA/custom_database",
+    url="https://github.com/SantiagoEnriqueGA/custom_database",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'segadb_cli=segadb.segadb_cli:app',
+        ],
+    },
 )
